997bbac41c69c74f3f2de2b1c438bc5f8cb28d67
Sady <sadydoucoure@gmail.com>
2022-09-08 15:49:14 -0400

    Adding Modal open/close  management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+
   return (
     <div>
-      <Cart/>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShow={showCartHandler} onHide={hideCartHandler} />
       <main>
         <Meals />
       </main>
