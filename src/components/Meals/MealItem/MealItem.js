7dca4c4e24f45061ccd75d4f51d38babc330bf51
Sady <sadydoucoure@gmail.com>
2022-09-08 23:15:25 -0400

    Using cart reducer to add items to cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,20 +1,31 @@
-import classes from "./MealItem.module.css"
-import MealItemForm from "./MealItemForm"
-const MealItem = (props)=>{
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+  const price = `$${props.price.toFixed(2)}`;
 
-    const price = `$${props.price.toFixed(2)}`
-    return (
-        <li className={classes.meal}>
-            <div>
-                <h3>{props.name}</h3>
-                <div className={classes.description}>{props.description}</div>
-                <div className={classes.price}>{price}</div>
-            </div>
-            <div>
-                <MealItemForm/>
-            </div>
-        </li>
-    )
-}
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
 
-export default MealItem
+export default MealItem;
