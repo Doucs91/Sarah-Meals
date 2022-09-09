cbf4595683a696a9522248892f05dc644e79fb9b
Sady <sadydoucoure@gmail.com>
2022-09-09 12:27:41 -0400

    Adding the logic for Add and Remove butoon of the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,9 +8,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (item) => {
+    cartCtx.removeItem(item)
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1})
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -21,7 +25,7 @@ const Cart = (props) => {
             name={item.name}
             amount={item.amount}
             price={item.price}
-            onRemove={cartItemRemoveHandler.bind(null,item.id)}
+            onRemove={cartItemRemoveHandler.bind(null,item)}
             onAdd={cartItemAddHandler.bind(null, item)}
           />
         );
